0bdbd7d567f9372e41ac31f35025acdf9b36e802
salammustafa728 <salammustafa728@gmail.com>
2021-09-14 09:16:57 +0300

    edit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,43 +1,68 @@
-import React, { Component } from 'react'
-import './Main.css';
-import Products from './Products';
-import Contact from './Contact';
-import Recommended from './Recommended';
-import Featured from './Featured';
+import React, { Component } from "react";
+import "./Main.css";
+import Products from "./Products";
+import Contact from "./Contact";
+import Recommended from "./Recommended";
+import Background from "./images/Background.png";
+import Featured from "./Featured";
 import { BsFillBagFill } from "react-icons/bs";
 export class Main extends Component {
-  
-    render() {
-        return (
-            <div>
-                <div class="hero" id="#Main1">
-                    <div class="tint"></div>
-                    <div class="claim"><h1 className="h1Wlc">Welcome to our store,</h1>  You Will Find everything you want and more</div>
-                   <div className="btns">
-                   <button className="btnShop" ><a href="#RecommenedDiv">Shop Now</a> <BsFillBagFill/></button>
-                    <button className="btnGet" ><a href="#Contact">Get in touch</a></button>
-                   </div>    
-                </div>
-                <div>
-                    <br></br>
-                    <br></br>
-                <p>EXPLORE AWESOME PRODUCTS</p>
-                   <h1 className="centerH1">RECOMMENDED FOR YOU</h1>
-                   <div className="recommenedDiv" id="RecommenedDiv"> <Recommended/></div>
-                   <br></br>
-                   <button className="btnView"><a href="#Product">View more</a></button>
-                </div>
-                <br></br>
-                <br></br>
-               <div className="grid2" id="product">  
-                 <Products />
-               </div>
-               <div  id="Contact" >
-                   <Contact />
-               </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div class="hero" id="#Main1">
+          <div class="tint"></div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <h1
+            class="display-4 font-weight-bold"
+            style={{ color: "white", textAlign: "center",marginTop:"15%" }}
+          >
+            Welcome to our store,
+          </h1>
+          <p class="font-italic mb-0">
+            You Will Find everything you want and more
+          </p>
+          <div className="btns">
+            <button className="btnShop">
+              <a href="#RecommenedDiv">Shop Now</a> <BsFillBagFill />
+            </button>
+            <button className="btnGet">
+              <a href="#Contact">Get in touch</a>
+            </button>
+          </div>
+        </div>
+        <div
+          class="jumbotron bg-cover text-white heroImg"
+        >
+        </div>
+        <div>
+          <br></br>
+          <br></br>
+
+          <p>EXPLORE AWESOME PRODUCTS</p>
+          <h1 className="centerH1">RECOMMENDED FOR YOU</h1>
+          <div className="recommenedDiv" id="RecommenedDiv">
+            {" "}
+            <Recommended />
+          </div>
+          <br></br>
+          <button className="btn btn-5">
+            <a href="#Product">View more</a>
+          </button>
+        </div>
+        <br></br>
+        <br></br>
+        <div className="grid2" id="product">
+          <Products />
+        </div>
+        <div id="Contact">
+          <Contact />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Main
+export default Main;
